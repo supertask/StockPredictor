4c3e677b2dcba73257d7d39f1f8adfeb9ea9e7bc
Tasuku TAKAHASHI <lightfox.task@gmail.com>
2023-11-11 17:21:40 +0900

    Almost done to fix output data

--- data_creator/fix_output.py
+++ data_creator/fix_output.py
@@ -26,6 +26,7 @@ def clean_and_save_data(input_path, drop_columns, result_conversion=None, additi
     data = pd.read_csv(input_path)
     data_cleaned = data.drop(columns=drop_columns)
     data_cleaned['公表日時'] = data_cleaned['公表日時'].apply(convert_date_format_revised)
+    data_cleaned.sort_values(by='公表日時', inplace=True)  # 公表日時でソート
 
     data_cleaned = data_cleaned[data_cleaned['結果'].notna() & data_cleaned['結果'].ne('') & data_cleaned['結果'].ne(' ')]
 
@@ -73,6 +74,18 @@ def convert_michigan_consumer_sentiment(input_path):
     clean_and_save_data(input_path, COMMON_DROP_OFF_COLUMNS, 
                         additional_processing=remove_whitespace_from_time)
 
+def convert_nikkei(input_path):
+    data = pd.read_csv(input_path)
+    data['Date'] = pd.to_datetime(data['Date']).dt.strftime('%Y-%m-%d') # 意味はないが、日付の形式を変更する
+    keys = ["Open", "High", "Low", "Close", "Adj Close", "Volume"]
+    for key in keys:
+        data[key] = pd.to_numeric(data[key], errors='coerce')
+    data.dropna(inplace=True)
+    data.sort_values(by='Date', inplace=True)  # 日付でソート
+
+    fixed_output_path = os.path.join(FIXED_OUTPUT_DIR, "fixed_" + os.path.basename(input_path))
+    data.to_csv(fixed_output_path, index=False)
+
 convert_cb_consumer_confidence("output/cb_consumer_confidence.csv")
 convert_percent_csv("output/core_cpi.csv")
 convert_percent_csv("output/core_durable_goods_orders.csv")
@@ -84,3 +97,4 @@ convert_k_value_csv("output/nonfarm_payrolls.csv")
 convert_percent_csv("output/personal_spending.csv")
 convert_percent_csv("output/retail_sales.csv")
 convert_percent_csv("output/unemployment_rate.csv")
+convert_nikkei("output/nikkei_225.csv")
