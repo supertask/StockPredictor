35f0062ded70e2b91f275dfcb7df799ee9bdcab0
Tasuku TAKAHASHI <lightfox.task@gmail.com>
2024-02-24 02:35:20 +0900

    TSVから会社情報を取得できるようにした。

--- jquants/company_info.py
+++ jquants/company_info.py
@@ -13,24 +13,34 @@ TSV_PATH = './input/company_codes.tsv'
 class CompanyExtra:
     company_code: str
     company_name: str
+    pred_datetime: datetime
+    pred_high_20: float
+    pred_low_20: float
     years_months_since_ipo: str
     ipo_date: datetime
     newest_financial_statement_datetime: datetime
     months_days_since_newest_financial_statement: int
-    datetime: datetime
-    pred_high_20: float
-    pred_low_20: float
+    sector17_code_name: str
+    sector33_code_name: str
     
-
-    def __str__(self):
-        return (f"会社コード: {self.company_code}, "
-                f"会社名: {self.company_name}, "
-                f"IPOからの経過期間: {self.years_months_since_ipo}, "
-                f"IPO日: {self.ipo_date.strftime('%Y-%m-%d')}, "
-                f"最新財務諸表日時: {self.newest_financial_statement_datetime.strftime('%Y-%m-%d %H:%M:%S')}, "
-                f"最新財務諸表からの経過月日: {self.months_days_since_newest_financial_statement}, "
-                f"予測高値20: {self.pred_high_20}, "
-                f"予測低値20: {self.pred_low_20}")
+    @classmethod
+    def get_header(self):
+        return ["会社コード", "会社名", "datetime", "予測高値20", "予測低値20", "IPOからの経過期間", "IPO日", "最新財務諸表日時", "最新財務諸表からの経過月日", "17業種", "33業種"]
+    
+    def get_info_in_list(self):
+        return [
+            self.company_code,
+            self.company_name,
+            self.pred_datetime,
+            self.pred_high_20,
+            self.pred_low_20,
+            self.years_months_since_ipo,
+            self.ipo_date,
+            self.newest_financial_statement_datetime,
+            self.months_days_since_newest_financial_statement,
+            self.sector17_code_name,
+            self.sector33_code_name,
+        ]
 
 class JQuantsExtra:
     def __init__(self):
@@ -56,7 +66,7 @@ class JQuantsExtra:
         df_codes = pd.read_csv(TSV_PATH, sep='\t')
         df_codes['code'] = df_codes['code'].apply(self.transform_code)
         unique_codes = df_codes['code'].unique()
-        print(unique_codes)
+        #print(unique_codes)
         return df[df['Code'].isin(unique_codes)]
         
     def get_ipo_and_newest_finance_datetime(self, company_code):
@@ -64,7 +74,7 @@ class JQuantsExtra:
         df['DisclosedDate'] = pd.to_datetime(df['DisclosedDate'])
         oldest_date = df['DisclosedDate'].min()
         difference = relativedelta(current_date, oldest_date)
-        years_months_since_ipo = (f"{difference.years}年, {difference.months}ヶ月, {difference.days}日")
+        years_months_since_ipo = (f"{difference.years}年{difference.months}ヶ月{difference.days}日")
         ipo_date = oldest_date
 
         newest_financial_statement_date = df['DisclosedDate'].max()
@@ -72,13 +82,15 @@ class JQuantsExtra:
         newest_financial_statement_datetime = datetime.combine(newest_financial_statement_date, datetime.strptime(newest_financial_statement_time, '%H:%M:%S').time())
 
         difference = relativedelta(current_date, newest_financial_statement_datetime)
-        months_days_since_newest_financial_statement = (f"{difference.years}年, {difference.months}ヶ月, {difference.days}日")
+        months_days_since_newest_financial_statement = (f"{difference.years}年{difference.months}ヶ月{difference.days}日")
 
         company_extra = CompanyExtra(
-            company_code = company_code, years_months_since_ipo = years_months_since_ipo, ipo_date = ipo_date,
+            company_code = company_code, company_name = "",
+            pred_datetime = None, pred_high_20 = 0, pred_low_20 = 0,
+            years_months_since_ipo = years_months_since_ipo, ipo_date = ipo_date,
             newest_financial_statement_datetime = newest_financial_statement_datetime,
             months_days_since_newest_financial_statement = months_days_since_newest_financial_statement,
-            datetime = None, pred_high_20 = 0, pred_low_20 = 0, company_name = ""
+            sector17_code_name = "", sector33_code_name = ""
         )
         
         return company_extra
@@ -89,24 +101,38 @@ class JQuantsExtra:
         company_scores['code'] = company_scores['code'].apply(self.transform_code)
         companies = []
         #df = self.client.get_fins_statements(code = code)
+        
+        company_df = self.client.get_listed_info()
 
         for index, company_score in company_scores.iterrows():
             # 追加情報を取得または計算する必要があります
             # 以下はダミーの値で、実際の値を設定する必要があります
             company_code = company_score['code']
+            jquants_company_info = company_df[company_df['Code'] == company_code]
             
             company_extra = self.get_ipo_and_newest_finance_datetime(company_code)
-            company_extra.datetime = pd.to_datetime(company_score['datetime'])
+            company_extra.pred_datetime = pd.to_datetime(company_score['datetime'])
             company_extra.pred_high_20 = company_score['pred_high_20']
             company_extra.pred_low_20 = company_score['pred_low_20']
-            company_extra.company_name = ""
+            company_extra.company_name = jquants_company_info['CompanyName'].iloc[0]
+            company_extra.sector17_code_name = jquants_company_info['Sector17CodeName'].iloc[0]
+            company_extra.sector33_code_name = jquants_company_info['Sector33CodeName'].iloc[0]
+
+            
+            # TODO: 以下の情報を取得したい
+            # https://jpx.gitbook.io/j-quants-ja/api-reference/listed_info
+            #"CompanyName": "日本取引所グループ",
+            #"Sector17CodeName": "金融（除く銀行）",
+            #"Sector33CodeName": "その他金融業",
+            # PER
+            # ストックビジネスかどうか
             
             companies.append(company_extra)
         return self.sort_companies(companies)
 
     def sort_companies(self, companies):
         # IPO日が最も新しい順、同じ場合は最後の決算日が最も新しい順にソート
-        return sorted(companies, key=lambda x: (x.ipo_date, x.newest_financial_statement_datetime), reverse=True)
+        return sorted(companies, key=lambda x: (x.newest_financial_statement_datetime, x.ipo_date), reverse=True)
 
 
 
@@ -115,8 +141,16 @@ def main():
     #year = j_extra.get_years_from_ipo()
     #print(year)
     companies = j_extra.get_companies()
+    company_rows = [CompanyExtra.get_header()]
     for company in companies:
-        print(company) 
+        company_rows.append(company.get_info_in_list())
+    tsv_output = "\n".join(["\t".join(map(str, row)) for row in company_rows])
+
+    with open('./output/companies.tsv', 'w') as wf:
+        wf.write(tsv_output)
+
+    print(tsv_output)
+
     
     ## jquantsapiから上場企業情報を取得
     #df_listed_info = client.get_listed_info()
